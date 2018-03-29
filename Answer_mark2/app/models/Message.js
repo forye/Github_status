aed7fd3126af2c4ab688f1a792ad43fbddbd775f
Idan Fonea <idan@datree.io>
2018-03-29 21:40:18 +0300

    git bugfix

--- Answer_mark2/app/models/Message.js
+++
@@ -1,7 +0,0 @@
-var mongoose = require('mongoose');
-
-module.exports = mongoose.model('Message', {
-	status : String,
-	body : String,
-	created_on : String
-});
--- Answer_mark2/config/database.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-	//url : 'mongodb://idanfonea:panda1@ds059682.mongolab.com:59682/github_status_fourier'
-	url : 'mongodb://bigPanda:panda1@ds059682.mongolab.com:59682/github_status_fourier'
-}
